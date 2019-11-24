88daef78e871eab0b155548508572fc2d8c161ff
Evan Bubniak <evan.bubniak@gmail.com>
2019-11-24 17:30:21 -0500

    refactored as class, implemented raw straight lines; fixed-num-point straight lines still buggy

--- main.py
+++ main.py
@@ -1,25 +1,139 @@
-from glaze import read_json
+import os
+import bezier
+import numpy as np
+from glaze import read_json, render
+import matplotlib.pyplot as plt
+import argparse
 
-arial = read_json("Ariel.json")
+parser = argparse.ArgumentParser()
+parser.add_argument("-o", "--output_format", nargs="*", default = ["png"],
+    help="Output format of all renderings. Any output format supported by matplotlib.pyplot.savefig works here. Tip: PNG for pixel image, EPS for vector.")
+parser.add_argument("-g", "--glyph", nargs="*", default = ["o"],
+    help = "list of glyphs to render")
+parser.add_argument("-p", "--points", nargs="?", type=int, default = 20,
+    help = "Num points to render")    
+args = parser.parse_args()
 
-A_glyph = arial[0][2]
+OUTPUT_FORMATS = args.output_format
 
-A_straight_line_glyph = make_straight_line_glyph(A_glyph)
+def get_contour_length(contour):
+    length = 0
+    transposed_contour = np.transpose(contour, axes=(0, 2, 1)).astype(np.float64)
+    for curve in transposed_contour:
+        curve = bezier.Curve(curve, degree=2)
+        length += curve.length
+    return length
 
-def make_straight_line_glyph(glyph):
-    '''
-    glyph assumed format: [#contours, #beziers, 3, 2, 1]
-    - get glyph and raster
-    (raster is to compare against)
-    - for each contour in the glyph:
-    - sample the curves and divide into set # of segments
-    - put off-curve points onto the line
-    1) generalization of beziers
-    2) fix off-curve points
-    '''
+def make_output_dirs():
+    output_types = ["raw", "raw-straight-line", "fixed-straight-line"]
+    for output_type in output_types:
+        output_path = os.path.join("outputs", output_type)
+        if not os.path.exists(output_path):
+            os.makedirs(output_path)
 
-# def fix_glyph
+class Glyph:
+    def __init__(self, font_name, char_name):
+        self.glyph = None
+        self.char_name = char_name
+        font_path = os.path.join("fonts", "json", "{}.json".format(font_name.lower()))
+        font = read_json(font_path)
+        for glyph in font:
+            if glyph[1] == char_name:
+                self.glyph = glyph
+        if self.glyph is None:
+            raise Exception("Character not found in font")
+        self.font_name = self.glyph[0]
+        self.contours = self.glyph[2]
+        self.num_contours = len(self.contours)
+
+
+    def render_raw_glyph(self, display_fig=False):
+        # Just renders directly using Glaze and saves it.
+        plt.figure()
+        render(self.contours)
+        for output_format in OUTPUT_FORMATS:
+            file_name = "{}-{}-raw-glyph.{}".format(self.font_name, self.char_name, output_format)
+            plt.savefig(os.path.join("outputs", "raw", file_name))
+        if display_fig:
+            plt.show()
+
+    def render_straight_line_glyph(self):
+        '''
+        glyph assumed format: [#contours, #beziers, 3, 2, 1]
+        - get glyph and raster
+        (raster is to compare against)
+        - for each contour in the glyph:
+        - sample the curves and divide into set # of segments
+        - put off-curve points onto the line
+        1) generalization of beziers
+        2) fix off-curve points
+        '''
+        contours = self.contours
+        for contour in contours:
+            for curve in contour:
+                for axis in range(len(curve[0])):
+                    curve[1][axis] = (curve[0][axis] + curve[2][axis])/2
+        plt.figure()
+        render(contours)
+        for output_format in OUTPUT_FORMATS:
+            file_name = "{}-{}-raw-straight-line-glyph.{}".format(self.font_name, self.char_name, output_format)
+            plt.savefig(os.path.join("outputs", "raw-straight-line", file_name))
+    
+    def render_fixed_num_bezier(self, num_points = 20):
+        fixed_num_contours = []
+        
+        for contour in self.contours:
+            fixed_num_contour = []
+            start_points = []
+            end_points = []
+
+            contour_len = get_contour_length(contour)
+            dist_per_point = contour_len/num_points
+            loc = 0
+            len_sum = 0
+            transposed_contour = np.transpose(contour, axes=(0, 2, 1)).astype(np.float64)
+
+            first = True
+            last = False
+
+            for curve in transposed_contour:
+                curve = bezier.Curve(curve, degree = 2)
+                if loc == contour_len:
+                    last = True
+                if loc <= len_sum:
+                    proportion = (loc - len_sum)/curve.length
+                    point = curve.evaluate(proportion).flatten()
+                    if first:
+                        start_points.append(point)
+                    elif last:
+                        end_points.append(point)
+                    else:
+                        start_points.append(point)
+                        end_points.append(point)
+                    loc += dist_per_point
+                len_sum += curve.length
+                first = False
+        
+            for start_point, end_point in zip(start_points, end_points):
+                off_point = [(start_point[0] + end_point[0])/2, (start_point[1] + end_point[1])/2]
+                curve = [start_point, off_point, end_point]
+                fixed_num_contour.append(curve)
+
+            fixed_num_contours.append(np.array(fixed_num_contour))
+
+        plt.figure()
+        render(fixed_num_contours)
+        for output_format in OUTPUT_FORMATS:
+            file_name = "{}-{}-fixed-straight-line-glyph.{}".format(self.font_name, self.char_name, output_format)
+            plt.savefig(os.path.join("outputs", "fixed-straight-line", file_name))        
+                    
+
+                
 
 if __name__ == "__main__":
-    # main script
-    pass
+    make_output_dirs()
+    for glyph in args.glyph:
+        glyph = Glyph(font_name="Arial", char_name=glyph)
+        glyph.render_raw_glyph()
+        glyph.render_straight_line_glyph()
+        glyph.render_fixed_num_bezier(num_points = args.points)
